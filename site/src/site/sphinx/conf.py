6b19c2e2289569fbe4e1dc00023f19369265626d
hengyunabc <hengyunabc@gmail.com>
2018-09-19 17:30:57 +0800

    exclude en dir. #56

--- site/src/site/sphinx/conf.py
+++ site/src/site/sphinx/conf.py
@@ -88,7 +88,7 @@ language = u'zh_CN'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store', u'en']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
