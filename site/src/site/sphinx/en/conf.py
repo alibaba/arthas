9f9b1a370ed0920ae4abe67e0bbb9a5491bea1cc
hengyunabc <hengyunabc@gmail.com>
2018-09-26 15:15:57 +0800

    fix en doc edit on github link.

--- site/src/site/sphinx/en/conf.py
+++ site/src/site/sphinx/en/conf.py
@@ -78,7 +78,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-# language = u'zh_CN'
+language = u'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -203,7 +203,7 @@ html_static_path = ['_static']
 # Sphinx supports the following languages:
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
+html_search_language = 'en'
 
 # A dictionary with options for the search language support, empty by default.
 # Now only 'ja' uses this config value
