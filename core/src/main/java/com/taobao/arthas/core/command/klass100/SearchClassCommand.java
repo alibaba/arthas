65fc3bf5837117443c6dc12b5e44588c147da864
hengyunabc <hengyunabc@gmail.com>
2018-10-18 19:46:13 +0800

    classloader command support load class. #246

--- core/src/main/java/com/taobao/arthas/core/command/klass100/SearchClassCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/klass100/SearchClassCommand.java
@@ -1,9 +1,7 @@
 package com.taobao.arthas.core.command.klass100;
 
-import static com.taobao.text.ui.Element.label;
 
 import java.lang.instrument.Instrumentation;
-import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,18 +10,15 @@ import java.util.List;
 import com.taobao.arthas.core.command.Constants;
 import com.taobao.arthas.core.shell.command.AnnotatedCommand;
 import com.taobao.arthas.core.shell.command.CommandProcess;
+import com.taobao.arthas.core.util.ClassUtils;
 import com.taobao.arthas.core.util.SearchUtils;
 import com.taobao.arthas.core.util.StringUtils;
-import com.taobao.arthas.core.util.TypeRenderUtils;
 import com.taobao.arthas.core.util.affect.RowAffect;
 import com.taobao.middleware.cli.annotations.Argument;
 import com.taobao.middleware.cli.annotations.Description;
 import com.taobao.middleware.cli.annotations.Name;
 import com.taobao.middleware.cli.annotations.Option;
 import com.taobao.middleware.cli.annotations.Summary;
-import com.taobao.text.Decoration;
-import com.taobao.text.ui.Element;
-import com.taobao.text.ui.TableElement;
 import com.taobao.text.util.RenderUtil;
 
 /**
@@ -45,14 +40,6 @@ public class SearchClassCommand extends AnnotatedCommand {
     private boolean isField = false;
     private boolean isRegEx = false;
     private Integer expand;
-
-    public static String getCodeSource(final CodeSource cs) {
-        if (null == cs || null == cs.getLocation() || null == cs.getLocation().getFile()) {
-            return com.taobao.arthas.core.util.Constants.EMPTY_STRING;
-        }
-
-        return cs.getLocation().getFile();
-    }
 
     @Argument(argName = "class-pattern", index = 0)
     @Description("Class name pattern, use either '.' or '/' as separator")
@@ -108,40 +95,10 @@ public class SearchClassCommand extends AnnotatedCommand {
 
     private void processClass(CommandProcess process, Class<?> clazz) {
         if (isDetail) {
-            process.write(RenderUtil.render(renderClassInfo(clazz, isField), process.width()) + "\n");
+            process.write(RenderUtil.render(ClassUtils.renderClassInfo(clazz, isField, expand), process.width()) + "\n");
         } else {
             process.write(clazz.getName() + "\n");
         }
-    }
-
-    private Element renderClassInfo(Class<?> clazz, boolean isPrintField) {
-        TableElement table = new TableElement().leftCellPadding(1).rightCellPadding(1);
-        CodeSource cs = clazz.getProtectionDomain().getCodeSource();
-
-        table.row(label("class-info").style(Decoration.bold.bold()), label(StringUtils.classname(clazz)))
-                .row(label("code-source").style(Decoration.bold.bold()), label(getCodeSource(cs)))
-                .row(label("name").style(Decoration.bold.bold()), label(StringUtils.classname(clazz)))
-                .row(label("isInterface").style(Decoration.bold.bold()), label("" + clazz.isInterface()))
-                .row(label("isAnnotation").style(Decoration.bold.bold()), label("" + clazz.isAnnotation()))
-                .row(label("isEnum").style(Decoration.bold.bold()), label("" + clazz.isEnum()))
-                .row(label("isAnonymousClass").style(Decoration.bold.bold()), label("" + clazz.isAnonymousClass()))
-                .row(label("isArray").style(Decoration.bold.bold()), label("" + clazz.isArray()))
-                .row(label("isLocalClass").style(Decoration.bold.bold()), label("" + clazz.isLocalClass()))
-                .row(label("isMemberClass").style(Decoration.bold.bold()), label("" + clazz.isMemberClass()))
-                .row(label("isPrimitive").style(Decoration.bold.bold()), label("" + clazz.isPrimitive()))
-                .row(label("isSynthetic").style(Decoration.bold.bold()), label("" + clazz.isSynthetic()))
-                .row(label("simple-name").style(Decoration.bold.bold()), label(clazz.getSimpleName()))
-                .row(label("modifier").style(Decoration.bold.bold()), label(StringUtils.modifier(clazz.getModifiers(), ',')))
-                .row(label("annotation").style(Decoration.bold.bold()), label(TypeRenderUtils.drawAnnotation(clazz)))
-                .row(label("interfaces").style(Decoration.bold.bold()), label(TypeRenderUtils.drawInterface(clazz)))
-                .row(label("super-class").style(Decoration.bold.bold()), TypeRenderUtils.drawSuperClass(clazz))
-                .row(label("class-loader").style(Decoration.bold.bold()), TypeRenderUtils.drawClassLoader(clazz))
-                .row(label("classLoaderHash").style(Decoration.bold.bold()), label(StringUtils.classLoaderHash(clazz)));
-
-        if (isPrintField) {
-            table.row(label("fields"), TypeRenderUtils.drawField(clazz, expand));
-        }
-        return table;
     }
 
 }
