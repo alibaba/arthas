8ff23ff2721b8acbee395a71881cc74ad82f73e0
hengyunabc <hengyunabc@gmail.com>
2018-11-09 01:44:37 +0800

    format code

--- client/src/main/java/com/taobao/arthas/client/OSUtils.java
+++ client/src/main/java/com/taobao/arthas/client/OSUtils.java
@@ -2,12 +2,12 @@ package com.taobao.arthas.client;
 
 public class OSUtils {
 
-	public static boolean isWindowsOS() {
-		boolean isWindowsOS = false;
-		String osName = System.getProperty("os.name");
-		if (osName.toLowerCase().indexOf("windows") > -1) {
-			isWindowsOS = true;
-		}
-		return isWindowsOS;
-	}
+    public static boolean isWindowsOS() {
+        boolean isWindowsOS = false;
+        String osName = System.getProperty("os.name");
+        if (osName.toLowerCase().indexOf("windows") > -1) {
+            isWindowsOS = true;
+        }
+        return isWindowsOS;
+    }
 }
