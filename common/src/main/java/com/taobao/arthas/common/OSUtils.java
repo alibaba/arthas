dc2cad5319db53e5c4285b11a1b752e87026656c
hengyunabc <hengyunabc@gmail.com>
2018-11-09 16:09:23 +0800

    merge two OSUtils

--- common/src/main/java/com/taobao/arthas/common/OSUtils.java
+++ common/src/main/java/com/taobao/arthas/common/OSUtils.java
@@ -1,22 +1,24 @@
 package com.taobao.arthas.common;
 
+import java.util.Locale;
+
 /**
  *
  * @author hengyunabc 2018-11-08
  *
  */
 public class OSUtils {
+    private static final String OPERATING_SYSTEM_NAME = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
 
     static PlatformEnum platform;
     static {
-        String osName = System.getProperty("os.name");
-        if (osName.startsWith("Linux")) {
+        if (OPERATING_SYSTEM_NAME.startsWith("Linux")) {
             platform = PlatformEnum.LINUX;
-        } else if (osName.startsWith("Mac") || osName.startsWith("Darwin")) {
+        } else if (OPERATING_SYSTEM_NAME.startsWith("Mac") || OPERATING_SYSTEM_NAME.startsWith("Darwin")) {
             platform = PlatformEnum.MACOSX;
-        } else if (osName.startsWith("Mac") || osName.startsWith("Darwin")) {
+        } else if (OPERATING_SYSTEM_NAME.startsWith("Mac") || OPERATING_SYSTEM_NAME.startsWith("Darwin")) {
             platform = PlatformEnum.MACOSX;
-        } else if (osName.startsWith("Windows")) {
+        } else if (OPERATING_SYSTEM_NAME.startsWith("Windows")) {
             platform = PlatformEnum.WINDOWS;
         } else {
             platform = PlatformEnum.UNKNOWN;
@@ -33,6 +35,16 @@ public class OSUtils {
 
     public static boolean isMac() {
         return platform == PlatformEnum.MACOSX;
+    }
+
+    public static boolean isCygwinOrMinGW() {
+        if (isWindows()) {
+            if ((System.getenv("MSYSTEM") != null && System.getenv("MSYSTEM").startsWith("MINGW"))
+                            || "/bin/shell".equals(System.getenv("SHELL"))) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
