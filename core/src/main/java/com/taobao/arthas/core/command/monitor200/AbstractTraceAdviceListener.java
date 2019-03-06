9e1ae9ba99ca4da451bc0f4fbef4418d09e2960b
w <w@hidva.com>
2019-03-06 15:00:47 +0800

    feat: 现在 trace 具有 watch command 具有的功能了

--- core/src/main/java/com/taobao/arthas/core/command/monitor200/AbstractTraceAdviceListener.java
+++ core/src/main/java/com/taobao/arthas/core/command/monitor200/AbstractTraceAdviceListener.java
@@ -4,8 +4,11 @@ import com.taobao.arthas.core.advisor.Advice;
 import com.taobao.arthas.core.advisor.ArthasMethod;
 import com.taobao.arthas.core.advisor.ReflectAdviceListenerAdapter;
 import com.taobao.arthas.core.shell.command.CommandProcess;
+import com.taobao.arthas.core.util.DateUtils;
 import com.taobao.arthas.core.util.LogUtil;
+import com.taobao.arthas.core.util.StringUtils;
 import com.taobao.arthas.core.util.ThreadLocalWatch;
+import com.taobao.arthas.core.view.ObjectView;
 
 /**
  * @author ralf0131 2017-01-06 16:02.
@@ -44,6 +47,13 @@ public class AbstractTraceAdviceListener extends ReflectAdviceListenerAdapter {
         threadBoundEntity.get().deep++;
         // 开始计算本次方法调用耗时
         threadLocalWatch.start();
+        if (command.enableWatch() && command.isBefore()) {
+            watching(Advice.newForBefore(loader, clazz, method, target, args));
+        }
+    }
+
+    private boolean isFinish() {
+        return command.isFinish() || !command.isBefore() && !command.isException() && !command.isSuccess();
     }
 
     @Override
@@ -52,6 +62,10 @@ public class AbstractTraceAdviceListener extends ReflectAdviceListenerAdapter {
         threadBoundEntity.get().view.end();
         final Advice advice = Advice.newForAfterRetuning(loader, clazz, method, target, args, returnObject);
         finishing(advice);
+        if (command.enableWatch() && (command.isSuccess() || isFinish())) {
+            // finishing() 中的 condition expression 存在修改 advice 状态可能性, 所以这里最好还是再 new 一个.
+            watching(Advice.newForAfterRetuning(loader, clazz, method, target, args, returnObject));
+        }
     }
 
     @Override
@@ -60,10 +74,40 @@ public class AbstractTraceAdviceListener extends ReflectAdviceListenerAdapter {
         threadBoundEntity.get().view.begin("throw:" + throwable.getClass().getName() + "()").end().end();
         final Advice advice = Advice.newForAfterThrowing(loader, clazz, method, target, args, throwable);
         finishing(advice);
+        if (command.enableWatch() && (command.isException() || isFinish())) {
+            // finishing() 中的 condition expression 存在修改 advice 状态可能性, 所以这里最好还是再 new 一个.
+            watching(Advice.newForAfterThrowing(loader, clazz, method, target, args, throwable));
+        }
     }
 
     public TraceCommand getCommand() {
         return command;
+    }
+
+    private boolean isNeedExpand() {
+        Integer expand = command.getExpand();
+        return null != expand && expand >= 0;
+    }
+
+    private void watching(Advice advice) {
+        try {
+            // 本次调用的耗时
+            double cost = threadLocalWatch.costInMillis();
+            if (isConditionMet(command.getConditionExpress(), advice, cost)) {
+                // TODO: concurrency issues for process.write
+                Object value = getExpressionResult(command.getExpress(), advice, cost);
+                String result = StringUtils.objectToString(
+                        isNeedExpand() ? new ObjectView(value, command.getExpand(), command.getSizeLimit()).draw() : value);
+                process.write("ts=" + DateUtils.getCurrentDate() + "; [cost=" + cost + "ms] result=" + result + "\n");
+            }
+        } catch (Exception e) {
+            LogUtil.getArthasLogger().warn("watch failed.", e);
+            process.write("watch failed, condition is: " + command.getConditionExpress() + ", express is: "
+                          + command.getExpress() + ", " + e.getMessage() + ", visit " + LogUtil.LOGGER_FILE
+                          + " for more details.\n");
+            process.end();
+        }
+        return;
     }
 
     private void finishing(Advice advice) {
--- core/src/main/java/com/taobao/arthas/core/command/monitor200/TraceCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/monitor200/TraceCommand.java
@@ -4,6 +4,7 @@ import com.taobao.arthas.core.advisor.AdviceListener;
 import com.taobao.arthas.core.command.Constants;
 import com.taobao.arthas.core.shell.command.CommandProcess;
 import com.taobao.arthas.core.util.SearchUtils;
+import com.taobao.arthas.core.util.StringUtils;
 import com.taobao.arthas.core.util.matcher.GroupMatcher;
 import com.taobao.arthas.core.util.matcher.Matcher;
 import com.taobao.arthas.core.util.matcher.RegexMatcher;
@@ -18,6 +19,7 @@ import com.taobao.middleware.cli.annotations.Summary;
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO: trace 可以看做是 watch 的超集, 所以或许可以合并 TraceCommand 链路与 WatchCommand 链路的重复代码.
 /**
  * 调用跟踪命令<br/>
  * 负责输出一个类中的所有方法调用路径
@@ -39,6 +41,13 @@ public class TraceCommand extends EnhancerCommand {
     private String classPattern;
     private String methodPattern;
     private String conditionExpress;
+    private String express;
+    private Integer expand = 1;
+    private Integer sizeLimit = 10 * 1024 * 1024;
+    private boolean isBefore = false;
+    private boolean isFinish = false;
+    private boolean isException = false;
+    private boolean isSuccess = false;
     private boolean isRegEx = false;
     private int numberOfLimit = 100;
     private List<String> pathPatterns;
@@ -60,6 +69,36 @@ public class TraceCommand extends EnhancerCommand {
     @Description(Constants.CONDITION_EXPRESS)
     public void setConditionExpress(String conditionExpress) {
         this.conditionExpress = conditionExpress;
+    }
+
+    @Argument(argName = "express", index=3, required = false)
+    @Description("the content you want to watch, written by ognl.\n" + Constants.EXPRESS_EXAMPLES)
+    public void setExpress(String express) {
+        this.express = express;
+    }
+
+    @Option(shortName = "b", longName = "before", flag = true)
+    @Description("Watch before invocation")
+    public void setBefore(boolean before) {
+        isBefore = before;
+    }
+
+    @Option(shortName = "f", longName = "finish", flag = true)
+    @Description("Watch after invocation, enable by default")
+    public void setFinish(boolean finish) {
+        isFinish = finish;
+    }
+
+    @Option(shortName = "e", longName = "exception", flag = true)
+    @Description("Watch after throw exception")
+    public void setException(boolean exception) {
+        isException = exception;
+    }
+
+    @Option(shortName = "s", longName = "success", flag = true)
+    @Description("Watch after successful invocation")
+    public void setSuccess(boolean success) {
+        isSuccess = success;
     }
 
     @Option(shortName = "E", longName = "regex", flag = true)
@@ -86,6 +125,26 @@ public class TraceCommand extends EnhancerCommand {
         this.skipJDKTrace = skipJDKTrace;
     }
 
+    @Option(shortName = "x", longName = "expand")
+    @Description("Expand level of object (1 by default)")
+    public void setExpand(Integer expand) {
+        this.expand = expand;
+    }
+
+    @Option(shortName = "M", longName = "sizeLimit")
+    @Description("Upper size limit in bytes for the result (10 * 1024 * 1024 by default)")
+    public void setSizeLimit(Integer sizeLimit) {
+    this.sizeLimit = sizeLimit;
+    }
+
+    public Integer getSizeLimit() {
+        return sizeLimit;
+    }
+
+    public Integer getExpand() {
+        return expand;
+    }
+
     public String getClassPattern() {
         return classPattern;
     }
@@ -98,8 +157,32 @@ public class TraceCommand extends EnhancerCommand {
         return conditionExpress;
     }
 
+    public boolean enableWatch () {
+        return !StringUtils.isEmpty(express);
+    }
+
+    public String getExpress() {
+        return express;
+    }
+
     public boolean isSkipJDKTrace() {
         return skipJDKTrace;
+    }
+
+    public boolean isBefore() {
+        return isBefore;
+    }
+
+    public boolean isFinish() {
+        return isFinish;
+    }
+
+    public boolean isException() {
+        return isException;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
     }
 
     public boolean isRegEx() {
