28d80c796325d939ac19c86e184cc9183a0eeac7
ZhiFeng Hu <hufeng1987@gmail.com>
2019-06-19 11:46:36 +0800

    Fix #744 Class-pattern should not be single '*', it may cause system hangs

--- core/src/main/java/com/taobao/arthas/core/command/monitor200/TimeTunnelCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/monitor200/TimeTunnelCommand.java
@@ -237,12 +237,15 @@ public class TimeTunnelCommand extends EnhancerCommand {
         }
 
         // 在t参数下class-pattern,method-pattern
-        if (isTimeTunnel) {
+        if (isTimeTunnel) {            
             if (StringUtils.isEmpty(classPattern)) {
                 throw new IllegalArgumentException("Class-pattern is expected, please type the wildcard expression to match");
             }
             if (StringUtils.isEmpty(methodPattern)) {
                 throw new IllegalArgumentException("Method-pattern is expected, please type the wildcard expression to match");
+            }
+            if (classPattern.equals("*")) {
+                throw new IllegalArgumentException("Class-pattern should not be single '*', it may cause system hangs");
             }
         }
 
