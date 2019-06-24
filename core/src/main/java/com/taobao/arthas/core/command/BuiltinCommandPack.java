d8285f84426ef711eb97da17bd5db240d3d8433d
欧洲游 <ouzhouyou@raycloud.com>
2019-06-24 18:19:23 +0800

    补充一个DNS工具用于排查线上DNS缓存问题

--- core/src/main/java/com/taobao/arthas/core/command/BuiltinCommandPack.java
+++ core/src/main/java/com/taobao/arthas/core/command/BuiltinCommandPack.java
@@ -1,43 +1,17 @@
 package com.taobao.arthas.core.command;
 
-import com.taobao.arthas.core.command.basic1000.CatCommand;
-import com.taobao.arthas.core.command.basic1000.ClsCommand;
-import com.taobao.arthas.core.command.basic1000.HelpCommand;
-import com.taobao.arthas.core.command.basic1000.HistoryCommand;
-import com.taobao.arthas.core.command.basic1000.KeymapCommand;
-import com.taobao.arthas.core.command.basic1000.PwdCommand;
-import com.taobao.arthas.core.command.basic1000.ResetCommand;
-import com.taobao.arthas.core.command.basic1000.SessionCommand;
-import com.taobao.arthas.core.command.basic1000.ShutdownCommand;
-import com.taobao.arthas.core.command.basic1000.SystemEnvCommand;
-import com.taobao.arthas.core.command.basic1000.SystemPropertyCommand;
-import com.taobao.arthas.core.command.basic1000.VersionCommand;
-import com.taobao.arthas.core.command.hidden.JulyCommand;
-import com.taobao.arthas.core.command.hidden.OptionsCommand;
-import com.taobao.arthas.core.command.hidden.ThanksCommand;
-import com.taobao.arthas.core.command.klass100.ClassLoaderCommand;
-import com.taobao.arthas.core.command.klass100.DumpClassCommand;
-import com.taobao.arthas.core.command.klass100.GetStaticCommand;
-import com.taobao.arthas.core.command.klass100.JadCommand;
-import com.taobao.arthas.core.command.klass100.MemoryCompilerCommand;
-import com.taobao.arthas.core.command.klass100.OgnlCommand;
-import com.taobao.arthas.core.command.klass100.RedefineCommand;
-import com.taobao.arthas.core.command.klass100.SearchClassCommand;
-import com.taobao.arthas.core.command.klass100.SearchMethodCommand;
-import com.taobao.arthas.core.command.monitor200.DashboardCommand;
-import com.taobao.arthas.core.command.monitor200.JvmCommand;
-import com.taobao.arthas.core.command.monitor200.MBeanCommand;
-import com.taobao.arthas.core.command.monitor200.MonitorCommand;
-import com.taobao.arthas.core.command.monitor200.StackCommand;
-import com.taobao.arthas.core.command.monitor200.ThreadCommand;
-import com.taobao.arthas.core.command.monitor200.TimeTunnelCommand;
-import com.taobao.arthas.core.command.monitor200.TraceCommand;
-import com.taobao.arthas.core.command.monitor200.WatchCommand;
-import com.taobao.arthas.core.shell.command.Command;
-import com.taobao.arthas.core.shell.command.CommandResolver;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.taobao.arthas.core.command.basic1000.*;
+import com.taobao.arthas.core.command.hidden.JulyCommand;
+import com.taobao.arthas.core.command.hidden.OptionsCommand;
+import com.taobao.arthas.core.command.hidden.ThanksCommand;
+import com.taobao.arthas.core.command.klass100.*;
+import com.taobao.arthas.core.command.monitor200.*;
+import com.taobao.arthas.core.shell.command.Command;
+import com.taobao.arthas.core.shell.command.CommandResolver;
 
 /**
  * TODO automatically discover the built-in commands.
@@ -91,5 +65,6 @@ public class BuiltinCommandPack implements CommandResolver {
         commands.add(Command.create(CatCommand.class));
         commands.add(Command.create(PwdCommand.class));
         commands.add(Command.create(MBeanCommand.class));
+        commands.add(Command.create(DnsCommand.class));
     }
 }
