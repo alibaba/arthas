7b25a9401663745fb04409c611aa47fce4ffeddb
zhangjin <zhangjin@yunzhangfang.com>
2019-09-16 16:28:37 +0800

    modify IllegalArgumentException message

--- tunnel-server/src/main/java/com/alibaba/arthas/tunnel/server/TunnelSocketFrameHandler.java
+++ tunnel-server/src/main/java/com/alibaba/arthas/tunnel/server/TunnelSocketFrameHandler.java
@@ -156,7 +156,7 @@ public class TunnelSocketFrameHandler extends SimpleChannelInboundHandler<WebSoc
         } else {
             tunnelSocketCtx.channel().writeAndFlush(new CloseWebSocketFrame(2000, "Can not find arthas agent by id: "+ agentId));
             logger.error("Can not find arthas agent by id: ", agentId);
-            throw new IllegalArgumentException("arthas agent id can not be null");
+            throw new IllegalArgumentException("Can not find arthas agent by id: " + agentId);
         }
     }
 
