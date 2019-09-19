3ad47a3944e1d5137140f7d0a684efa37e173d85
hengyunabc <hengyunabc@hengyunabc-mac.local>
2019-09-19 10:42:25 +0800

    update doc 3.1.3

--- en/_static/documentation_options.js
+++ en/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.2',
+    VERSION: '3.1.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
