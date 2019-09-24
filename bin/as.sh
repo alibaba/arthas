8062e32977667482d71ce3b69aa7c848652e1c5f
hengyunabc <hengyunabc@gmail.com>
2019-09-25 02:09:31 +0800

    prepare release

--- bin/as.sh
+++ bin/as.sh
@@ -11,7 +11,7 @@
 #    date : 2019-09-19
 
 # current arthas script version
-ARTHAS_SCRIPT_VERSION=3.1.3
+ARTHAS_SCRIPT_VERSION=3.1.4
 
 # SYNOPSIS
 #   rreadlink <fileOrDirPath>
@@ -440,7 +440,7 @@ EXAMPLES:
   ./as.sh --stat-url 'http://192.168.10.11:8080/api/stat'
   ./as.sh -c 'sysprop; thread' <pid>
   ./as.sh -f batch.as <pid>
-  ./as.sh --use-version 3.1.3
+  ./as.sh --use-version 3.1.4
   ./as.sh --session-timeout 3600
   ./as.sh --attach-only
   ./as.sh --repo-mirror aliyun --use-http
