90d44ff15fb2533cbb12c9023c46d54e20b618db
qxo <49526356@qq.com>
2019-10-20 18:06:49 +0800

    improve history command with uniq(-u) history and clear history with regexp pattern

--- core/src/main/java/com/taobao/arthas/core/command/basic1000/HistoryCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/basic1000/HistoryCommand.java
@@ -1,7 +1,10 @@
 package com.taobao.arthas.core.command.basic1000;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 import com.taobao.arthas.core.command.Constants;
 import com.taobao.arthas.core.shell.command.AnnotatedCommand;
@@ -24,10 +27,22 @@ import io.termd.core.util.Helper;
  */
 @Name("history")
 @Summary("Display command history")
-@Description(Constants.EXAMPLE + "  history\n" + "  history -c\n" + "  history 5\n")
+@Description(Constants.EXAMPLE + "  history\n" + "  history -c\n" + "  history 5\n"
+   +"  history -c -p history\n"
+   +"  history -u 10 \n"
+   +"  history -c -p '(history|shutdown|stop|exit)' -u 10\n"
+    )
 public class HistoryCommand extends AnnotatedCommand {
     boolean clear = false;
     int n = -1;
+    private boolean uniq = false;
+    private Pattern  pattern;
+
+    @Option( shortName = "p", longName = "pattern")
+    @Description("command pattern to match")
+    public void setPattern(String pattern) {
+        this.pattern =  pattern == null || pattern.isEmpty() ? null :  Pattern.compile(pattern);
+    }
 
     @Option(shortName = "c", longName = "clear")
     @Description("clear history")
@@ -35,6 +50,12 @@ public class HistoryCommand extends AnnotatedCommand {
         this.clear = clear;
     }
 
+    @Option(shortName = "u", longName = "uniq")
+    @Description("uniq history")
+    public void setUniq(boolean uniq) {
+        this.uniq = uniq;
+    }
+    
     @Argument(index = 0, argName = "n", required = false)
     @Description("how many history commnads to display")
     public void setNumber(int n) {
@@ -49,28 +70,71 @@ public class HistoryCommand extends AnnotatedCommand {
             TermImpl term = (TermImpl) termObject;
             Readline readline = term.getReadline();
             List<int[]> history = readline.getHistory();
-
             if (clear) {
-                readline.setHistory(new ArrayList<int[]>());
+                final List<int[]> newHistory = new ArrayList<int[]>();
+                int clearCount = 0;
+                final int ulimit = uniq && n > 0 ? n  : 0;
+                if (pattern != null || ( ulimit > 0) ) {
+                  List<String> cmdList = new ArrayList<String>();
+                  for(int[] line : history) {
+                      final String cmd = toCmd(line).trim();
+                      boolean flag = pattern != null ? pattern.matcher(cmd).find()  : false;
+                      if (!flag && (ulimit > 0)) {
+                           final int cSize = cmdList.size();
+                           final List<String> list = cSize <= ulimit  ? cmdList :   cmdList.subList(cSize -ulimit ,cSize);
+                           flag = list.contains(cmd);
+                      }
+                      if (flag) {
+                        clearCount ++;
+                      } else {
+                        cmdList.add(cmd);
+                        newHistory.add(line);
+                      }
+                  }
+                } else {
+                  clearCount = history.size();
+                }
+                process.write("clear history count:"+clearCount+"\n");
+                readline.setHistory(newHistory); 
+            } else if (uniq) {
+              int size = history.size();
+              if (n < 0) {
+                  n = size;
+              }
+              Set<String> cmdList = new LinkedHashSet<String>();
+              for (int i = 0; i < n; ++i) {
+                  int[] line = history.get(n - i - 1);
+                  final String cmd = toCmd(line);
+                  cmdList.add(cmd);
+              }
+              StringBuilder sb = new StringBuilder();
+              for (String c : cmdList) {
+                sb.append(c).append('\n');
+              }
+              process.write(sb.toString());
             } else {
                 StringBuilder sb = new StringBuilder();
-
                 int size = history.size();
                 if (n < 0) {
                     n = size;
                 }
-
                 for (int i = 0; i < n; ++i) {
                     int[] line = history.get(n - i - 1);
                     sb.append(String.format("%5s  ", size - (n - i - 1)));
                     Helper.appendCodePoints(line, sb);
                     sb.append('\n');
                 }
-
                 process.write(sb.toString());
             }
         }
 
         process.end();
+    }
+
+    protected String toCmd(int[] line) {
+      StringBuilder sb = new StringBuilder();
+      Helper.appendCodePoints(line, sb);
+      final String cmd = sb.toString();
+      return cmd;
     }
 }
