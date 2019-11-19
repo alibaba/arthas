d74baccb4842d8b2e1054d0d5bf72cbf08641432
Huang Jing <huangjing0260@hhdd.com>
2019-11-19 17:15:30 +0800

    monitor 调用链上的多个method被调用时的RT时间监控不准的问题

--- core/src/main/java/com/taobao/arthas/core/util/ThreadLocalWatch.java
+++ core/src/main/java/com/taobao/arthas/core/util/ThreadLocalWatch.java
@@ -1,5 +1,7 @@
 package com.taobao.arthas.core.util;
 
+import java.util.Stack;
+
 /**
  * 简单的调用计时器。TODO 用stack的实现更合理
  * Created by vlinux on 16/6/1.
@@ -7,25 +9,25 @@ package com.taobao.arthas.core.util;
  */
 public class ThreadLocalWatch {
 
-    private final ThreadLocal<Long> timestampRef = new ThreadLocal<Long>() {
+    private final ThreadLocal<Stack<Long>> timestampRef = new ThreadLocal<Stack<Long>>() {
         @Override
-        protected Long initialValue() {
-            return System.nanoTime();
+        protected Stack<Long> initialValue() {
+            return new Stack<Long>();
         }
     };
 
     public long start() {
         final long timestamp = System.nanoTime();
-        timestampRef.set(timestamp);
+        timestampRef.get().push(timestamp);
         return timestamp;
     }
 
     public long cost() {
-        return (System.nanoTime() - timestampRef.get());
+        return (System.nanoTime() - timestampRef.get().pop());
     }
 
     public double costInMillis() {
-        return (System.nanoTime() - timestampRef.get()) / 1000000.0;
+        return (System.nanoTime() - timestampRef.get().pop()) / 1000000.0;
     }
 
     public void clear() {
