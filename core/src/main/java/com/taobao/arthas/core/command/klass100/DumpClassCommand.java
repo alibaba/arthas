737d7eca3f14f592d1b1f7daa04cef8bc88f1f67
hengyunabc <hengyunabc@gmail.com>
2020-02-17 02:01:57 +0800

    fix dump -E option support. #1028

--- core/src/main/java/com/taobao/arthas/core/command/klass100/DumpClassCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/klass100/DumpClassCommand.java
@@ -67,7 +67,7 @@ public class DumpClassCommand extends AnnotatedCommand {
         this.code = code;
     }
 
-    @Option(shortName = "E", longName = "regex")
+    @Option(shortName = "E", longName = "regex", flag = true)
     @Description("Enable regular expression to match (wildcard matching by default)")
     public void setRegEx(boolean regEx) {
         isRegEx = regEx;
