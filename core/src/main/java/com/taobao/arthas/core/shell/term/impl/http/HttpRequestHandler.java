b6d55d0b0bca5c7a88b0ce0229daec0b8a485e99
kylixs <kylixs@qq.com>
2020-03-18 10:33:50 +0800

    refactor http read resource file

--- core/src/main/java/com/taobao/arthas/core/shell/term/impl/http/HttpRequestHandler.java
+++ core/src/main/java/com/taobao/arthas/core/shell/term/impl/http/HttpRequestHandler.java
@@ -1,6 +1,7 @@
 package com.taobao.arthas.core.shell.term.impl.http;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
@@ -61,56 +62,64 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
                 path = "/index.html";
             }
 
-            InputStream in = null;
             try {
-
                 DefaultFullHttpResponse fileViewResult = DirectoryBrowser.view(dir, path, request.protocolVersion());
-
                 if (fileViewResult != null) {
                     response = fileViewResult;
                 } else {
-                    URL res = HttpTtyConnection.class.getResource("/com/taobao/arthas/core/http" + path);
-                    if (res != null) {
-                        DefaultFullHttpResponse fullResp = new DefaultFullHttpResponse(request.protocolVersion(),
-                                HttpResponseStatus.OK);
-                        in = res.openStream();
-                        byte[] tmp = new byte[256];
-                        for (int l = 0; l != -1; l = in.read(tmp)) {
-                            fullResp.content().writeBytes(tmp, 0, l);
-                        }
-                        int li = path.lastIndexOf('.');
-                        if (li != -1 && li != path.length() - 1) {
-                            String ext = path.substring(li + 1, path.length());
-                            String contentType;
-                            if ("html".equals(ext)) {
-                                contentType = "text/html";
-                            } else if ("js".equals(ext)) {
-                                contentType = "application/javascript";
-                            } else if ("css".equals(ext)) {
-                                contentType = "text/css";
-                            } else {
-                                contentType = null;
-                            }
-
-                            if (contentType != null) {
-                                fullResp.headers().set(HttpHeaderNames.CONTENT_TYPE, contentType);
-                            }
-                        }
+                    FullHttpResponse fullResp = readFileFromResource(request, path);
+                    if(fullResp != null){
                         response = fullResp;
                     } else {
                         response.setStatus(HttpResponseStatus.NOT_FOUND);
                     }
                 }
-
             } catch (Throwable e) {
                 logger.error("arthas", "arthas process http request error: " + request.uri(), e);
             } finally {
                 ctx.write(response);
                 ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
                 future.addListener(ChannelFutureListener.CLOSE);
-                IOUtils.close(in);
             }
         }
+    }
+
+    private FullHttpResponse readFileFromResource(FullHttpRequest request, String path) throws IOException {
+        DefaultFullHttpResponse fullResp = null;
+        InputStream in = null;
+        try {
+            URL res = HttpTtyConnection.class.getResource("/com/taobao/arthas/core/http" + path);
+            if (res != null) {
+                fullResp = new DefaultFullHttpResponse(request.protocolVersion(),
+                        HttpResponseStatus.OK);
+                in = res.openStream();
+                byte[] tmp = new byte[256];
+                for (int l = 0; l != -1; l = in.read(tmp)) {
+                    fullResp.content().writeBytes(tmp, 0, l);
+                }
+                int li = path.lastIndexOf('.');
+                if (li != -1 && li != path.length() - 1) {
+                    String ext = path.substring(li + 1, path.length());
+                    String contentType;
+                    if ("html".equals(ext)) {
+                        contentType = "text/html";
+                    } else if ("js".equals(ext)) {
+                        contentType = "application/javascript";
+                    } else if ("css".equals(ext)) {
+                        contentType = "text/css";
+                    } else {
+                        contentType = null;
+                    }
+
+                    if (contentType != null) {
+                        fullResp.headers().set(HttpHeaderNames.CONTENT_TYPE, contentType);
+                    }
+                }
+            }
+        } finally {
+            IOUtils.close(in);
+        }
+        return fullResp;
     }
 
     private static void send100Continue(ChannelHandlerContext ctx) {
