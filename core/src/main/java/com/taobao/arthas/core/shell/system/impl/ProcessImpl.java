cdff0638ddc9114bf4527e06a7f054f0e148d138
kylixs <kylixs@qq.com>
2020-04-09 00:23:43 +0800

    Abort output when the job is interrupted

--- core/src/main/java/com/taobao/arthas/core/shell/system/impl/ProcessImpl.java
+++ core/src/main/java/com/taobao/arthas/core/shell/system/impl/ProcessImpl.java
@@ -588,6 +588,10 @@ public class ProcessImpl implements Process {
 
         @Override
         public void appendResult(ResultModel result) {
+            if (processStatus != ExecStatus.RUNNING) {
+                throw new IllegalStateException(
+                        "Cannot write to standard output when " + status().name().toLowerCase());
+            }
             ProcessImpl.this.appendResult(result);
         }
     }
