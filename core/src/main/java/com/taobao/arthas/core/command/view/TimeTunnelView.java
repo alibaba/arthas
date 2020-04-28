af2411d9945d62a614241fc4c30d8caffe750f6e
gongdewei <kylixs@qq.com>
2020-04-28 11:28:06 +0800

    add comment

--- core/src/main/java/com/taobao/arthas/core/command/view/TimeTunnelView.java
+++ core/src/main/java/com/taobao/arthas/core/command/view/TimeTunnelView.java
@@ -14,6 +14,7 @@ import static com.taobao.arthas.core.command.monitor200.TimeTunnelTable.*;
 import static java.lang.String.format;
 
 /**
+ * Term view for TimeTunnelCommand
  * @author gongdewei 2020/4/27
  */
 public class TimeTunnelView extends ResultView<TimeTunnelModel> {
@@ -21,41 +22,51 @@ public class TimeTunnelView extends ResultView<TimeTunnelModel> {
     @Override
     public void draw(CommandProcess process, TimeTunnelModel timeTunnelModel) {
         Integer expand = timeTunnelModel.getExpand();
+        boolean isNeedExpand = isNeedExpand(expand);
         Integer sizeLimit = timeTunnelModel.getSizeLimit();
+
         if (timeTunnelModel.getTimeFragmentList() != null) {
+            //show list table: tt -l / tt -t
             Element table = drawTimeTunnelTable(timeTunnelModel.getTimeFragmentList(), timeTunnelModel.getFirst());
             process.write(RenderUtil.render(table, process.width()));
 
         } else if (timeTunnelModel.getTimeFragment() != null) {
+            //show detail of single TimeFragment: tt -i 1000
             TimeFragmentVO tf = timeTunnelModel.getTimeFragment();
             TableElement table = TimeTunnelTable.createDefaultTable();
             TimeTunnelTable.drawTimeTunnel(table, tf);
-            TimeTunnelTable.drawParameters(table, tf.getParams(), isNeedExpand(expand), expand);
-            TimeTunnelTable.drawReturnObj(table, tf, isNeedExpand(expand), expand, sizeLimit);
-            TimeTunnelTable.drawThrowException(table, tf, isNeedExpand(expand), expand);
+            TimeTunnelTable.drawParameters(table, tf.getParams(), isNeedExpand, expand);
+            TimeTunnelTable.drawReturnObj(table, tf, isNeedExpand, expand, sizeLimit);
+            TimeTunnelTable.drawThrowException(table, tf, isNeedExpand, expand);
             process.write(RenderUtil.render(table, process.width()));
+
         } else if (timeTunnelModel.getWatchValue() != null) {
+            //watch single TimeFragment: tt -i 1000 -w 'params'
             Object value = timeTunnelModel.getWatchValue();
-            if (isNeedExpand(expand)) {
+            if (isNeedExpand) {
                 process.write(new ObjectView(value, expand, sizeLimit).draw()).write("\n");
             } else {
                 process.write(StringUtils.objectToString(value)).write("\n");
             }
+
         } else if (timeTunnelModel.getWatchResults() != null) {
+            //search & watch: tt -s 'returnObj!=null' -w 'returnObj'
             TableElement table = TimeTunnelTable.createDefaultTable();
             TimeTunnelTable.drawWatchTableHeader(table);
-            TimeTunnelTable.drawWatchResults(table, timeTunnelModel.getWatchResults(), isNeedExpand(expand), expand, sizeLimit);
+            TimeTunnelTable.drawWatchResults(table, timeTunnelModel.getWatchResults(), isNeedExpand, expand, sizeLimit);
             process.write(RenderUtil.render(table, process.width()));
+
         } else if (timeTunnelModel.getReplayResult() != null) {
+            //replay: tt -i 1000 -p
             TimeFragmentVO replayResult = timeTunnelModel.getReplayResult();
             Integer replayNo = timeTunnelModel.getReplayNo();
             TableElement table = TimeTunnelTable.createDefaultTable();
             TimeTunnelTable.drawPlayHeader(replayResult.getClassName(), replayResult.getMethodName(), replayResult.getObject(), replayResult.getIndex(), table);
-            TimeTunnelTable.drawParameters(table, replayResult.getParams(), isNeedExpand(expand), expand);
+            TimeTunnelTable.drawParameters(table, replayResult.getParams(), isNeedExpand, expand);
             if (replayResult.isReturn()) {
-                TimeTunnelTable.drawPlayResult(table, replayResult.getReturnObj(), isNeedExpand(expand), expand, sizeLimit, replayResult.getCost());
+                TimeTunnelTable.drawPlayResult(table, replayResult.getReturnObj(), isNeedExpand, expand, sizeLimit, replayResult.getCost());
             } else {
-                TimeTunnelTable.drawPlayException(table, replayResult.getThrowExp(), isNeedExpand(expand), expand);
+                TimeTunnelTable.drawPlayException(table, replayResult.getThrowExp(), isNeedExpand, expand);
             }
             process.write(RenderUtil.render(table, process.width()))
                     .write(format("Time fragment[%d] successfully replayed %d times.", replayResult.getIndex(), replayNo))
