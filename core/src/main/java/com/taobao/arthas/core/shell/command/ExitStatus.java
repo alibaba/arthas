f16e8079c852ff3d4f7b88214cbe0fbcb4120168
gongdewei <kylixs@qq.com>
2020-07-09 10:48:56 +0800

    remove unused code

--- core/src/main/java/com/taobao/arthas/core/shell/command/ExitStatus.java
+++ core/src/main/java/com/taobao/arthas/core/shell/command/ExitStatus.java
@@ -11,16 +11,6 @@ public class ExitStatus {
     public static final ExitStatus SUCCESS_STATUS = new ExitStatus(0);
 
     /**
-     * 异步执行的命令返回此状态，注意避免状态码冲突
-     */
-    public static final ExitStatus PENDING_STATUS = new ExitStatus(0x300000);
-
-    /**
-     * TODO 暂时兼容老代码使用，忽略检查此命令的status，命令改造完毕后需要移除此变量
-     */
-    public static final ExitStatus IGNORED_STATUS = new ExitStatus(0x100000);
-
-    /**
      * 命令执行成功的状态
      * @return
      */
@@ -38,9 +28,6 @@ public class ExitStatus {
         if (statusCode == 0) {
             throw new IllegalArgumentException("failure status code cannot be 0");
         }
-        if (statusCode == PENDING_STATUS.statusCode) {
-            throw new IllegalArgumentException("failure status cannot equals to PENDING_STATUS");
-        }
         return new ExitStatus(statusCode, message);
     }
 
@@ -53,23 +40,6 @@ public class ExitStatus {
         return exitStatus != null && exitStatus.getStatusCode() != 0;
     }
 
-    /**
-     * 判断是否为异步执行等待状态
-     * @param exitStatus
-     * @return
-     */
-    public static boolean isPending(ExitStatus exitStatus) {
-        return exitStatus != null && exitStatus.getStatusCode() == PENDING_STATUS.getStatusCode();
-    }
-
-    /**
-     * TODO 判断是否为IGNORE状态，兼容老代码使用，改造完毕后需要移除
-     * @param exitStatus
-     * @return
-     */
-    public static boolean isIgnored(ExitStatus exitStatus) {
-        return exitStatus != null && exitStatus.getStatusCode() == IGNORED_STATUS.getStatusCode();
-    }
 
     private int statusCode;
     private String message;
