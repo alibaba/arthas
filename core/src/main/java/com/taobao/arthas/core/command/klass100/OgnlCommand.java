2ffaa162af9b79438ecc4da0334903079310e0e3
gongdewei <kylixs@qq.com>
2020-07-23 17:53:33 +0800

    Match classloader by class name

--- core/src/main/java/com/taobao/arthas/core/command/klass100/OgnlCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/klass100/OgnlCommand.java
@@ -1,6 +1,7 @@
 package com.taobao.arthas.core.command.klass100;
 
 import java.lang.instrument.Instrumentation;
+import java.util.List;
 
 import com.alibaba.arthas.deps.org.slf4j.Logger;
 import com.alibaba.arthas.deps.org.slf4j.LoggerFactory;
@@ -37,8 +38,8 @@ public class OgnlCommand extends AnnotatedCommand {
     private static final Logger logger = LoggerFactory.getLogger(OgnlCommand.class);
 
     private String express;
-
     private String hashCode;
+    private String classLoaderClass;
     private int expand = 1;
 
     @Argument(argName = "express", index = 0, required = true)
@@ -53,6 +54,12 @@ public class OgnlCommand extends AnnotatedCommand {
         this.hashCode = hashCode;
     }
 
+    @Option(longName = "classLoaderClass")
+    @Description("The class name of the special class's classLoader, default is null")
+    public void setClassLoaderClass(String classLoaderClass) {
+        this.classLoaderClass = classLoaderClass;
+    }
+
     @Option(shortName = "x", longName = "expand")
     @Description("Expand level of object (1 by default).")
     public void setExpand(Integer expand) {
@@ -63,10 +70,18 @@ public class OgnlCommand extends AnnotatedCommand {
     public void process(CommandProcess process) {
         Instrumentation inst = process.session().getInstrumentation();
         ClassLoader classLoader = null;
-        if (hashCode == null) {
-            classLoader = ClassLoader.getSystemClassLoader();
-        } else {
+        if (hashCode != null) {
             classLoader = ClassLoaderUtils.getClassLoader(inst, hashCode);
+        } else if (classLoaderClass != null) {
+            List<ClassLoader> matchedClassLoaders = ClassLoaderUtils.getClassLoaderByClassName(inst, classLoaderClass);
+            if (matchedClassLoaders.size() > 1) {
+                process.end(-1, "Find more than one classloader matching the specified class name : " + classLoaderClass);
+                return;
+            } else if (matchedClassLoaders.size() == 1) {
+                classLoader = matchedClassLoaders.get(0);
+            }
+        } else {
+            classLoader = ClassLoader.getSystemClassLoader();
         }
 
         if (classLoader == null) {
