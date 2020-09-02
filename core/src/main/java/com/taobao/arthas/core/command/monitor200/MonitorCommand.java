cd67c4aa6626247849f4cec4b04ac8afb843b3b2
mikawudi <mikawudi@qq.com>
2020-09-02 02:36:05 -0500

    Add condition-express param to monitor command (#1420)

--- core/src/main/java/com/taobao/arthas/core/command/monitor200/MonitorCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/monitor200/MonitorCommand.java
@@ -23,15 +23,19 @@ import com.taobao.middleware.cli.annotations.Summary;
 @Description("\nExamples:\n" +
         "  monitor org.apache.commons.lang.StringUtils isBlank\n" +
         "  monitor org.apache.commons.lang.StringUtils isBlank -c 5\n" +
+        "  monitor org.apache.commons.lang.StringUtils isBlank params[0]!=null\n" +
+        "  monitor -b org.apache.commons.lang.StringUtils isBlank params[0]!=null\n" +
         "  monitor -E org\\.apache\\.commons\\.lang\\.StringUtils isBlank\n" +
         Constants.WIKI + Constants.WIKI_HOME + "monitor")
 public class MonitorCommand extends EnhancerCommand {
 
     private String classPattern;
     private String methodPattern;
+    private String conditionExpress;
     private int cycle = 60;
     private boolean isRegEx = false;
     private int numberOfLimit = 100;
+    private boolean isBefore = false;
 
     @Argument(argName = "class-pattern", index = 0)
     @Description("Path and classname of Pattern Matching")
@@ -43,6 +47,12 @@ public class MonitorCommand extends EnhancerCommand {
     @Description("Method of Pattern Matching")
     public void setMethodPattern(String methodPattern) {
         this.methodPattern = methodPattern;
+    }
+
+    @Argument(argName = "condition-express", index = 2, required = false)
+    @Description(Constants.CONDITION_EXPRESS)
+    public void setConditionExpress(String conditionExpress) {
+        this.conditionExpress = conditionExpress;
     }
 
     @Option(shortName = "c", longName = "cycle")
@@ -63,12 +73,22 @@ public class MonitorCommand extends EnhancerCommand {
         this.numberOfLimit = numberOfLimit;
     }
 
+    @Option(shortName = "b", longName = "before", flag = true)
+    @Description("Evaluate the condition-express before method invoke")
+    public void setBefore(boolean before) {
+        isBefore = before;
+    }
+
     public String getClassPattern() {
         return classPattern;
     }
 
     public String getMethodPattern() {
         return methodPattern;
+    }
+
+    public String getConditionExpress() {
+        return conditionExpress;
     }
 
     public int getCycle() {
@@ -81,6 +101,10 @@ public class MonitorCommand extends EnhancerCommand {
 
     public int getNumberOfLimit() {
         return numberOfLimit;
+    }
+
+    public boolean isBefore() {
+        return isBefore;
     }
 
     @Override
