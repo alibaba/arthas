2af84165e4135893bc7d39f9cdba23a4e008c874
kaixinbaba <452914639@qq.com>
2020-09-11 14:47:15 +0800

    clean code

--- core/src/main/java/com/taobao/arthas/core/command/BuiltinCommandPack.java
+++ core/src/main/java/com/taobao/arthas/core/command/BuiltinCommandPack.java
@@ -1,63 +1,18 @@
 package com.taobao.arthas.core.command;
 
-import com.taobao.arthas.common.AnsiLog;
-import com.taobao.arthas.core.command.basic1000.CatCommand;
-import com.taobao.arthas.core.command.basic1000.ClsCommand;
-import com.taobao.arthas.core.command.basic1000.EchoCommand;
-import com.taobao.arthas.core.command.basic1000.GrepCommand;
-import com.taobao.arthas.core.command.basic1000.HelpCommand;
-import com.taobao.arthas.core.command.basic1000.HistoryCommand;
-import com.taobao.arthas.core.command.basic1000.KeymapCommand;
-import com.taobao.arthas.core.command.basic1000.OptionsCommand;
-import com.taobao.arthas.core.command.basic1000.PwdCommand;
-import com.taobao.arthas.core.command.basic1000.ResetCommand;
-import com.taobao.arthas.core.command.basic1000.SessionCommand;
-import com.taobao.arthas.core.command.basic1000.ShutdownCommand;
-import com.taobao.arthas.core.command.basic1000.StopCommand;
-import com.taobao.arthas.core.command.basic1000.SystemEnvCommand;
-import com.taobao.arthas.core.command.basic1000.SystemPropertyCommand;
-import com.taobao.arthas.core.command.basic1000.TeeCommand;
-import com.taobao.arthas.core.command.basic1000.VMOptionCommand;
-import com.taobao.arthas.core.command.basic1000.VersionCommand;
-import com.taobao.arthas.core.command.hidden.JulyCommand;
-import com.taobao.arthas.core.command.hidden.ThanksCommand;
-import com.taobao.arthas.core.command.klass100.ClassLoaderCommand;
-import com.taobao.arthas.core.command.klass100.DumpClassCommand;
-import com.taobao.arthas.core.command.klass100.GetStaticCommand;
-import com.taobao.arthas.core.command.klass100.JadCommand;
-import com.taobao.arthas.core.command.klass100.MemoryCompilerCommand;
-import com.taobao.arthas.core.command.klass100.OgnlCommand;
-import com.taobao.arthas.core.command.klass100.RedefineCommand;
-import com.taobao.arthas.core.command.klass100.SearchClassCommand;
-import com.taobao.arthas.core.command.klass100.SearchMethodCommand;
-import com.taobao.arthas.core.command.logger.LoggerCommand;
-import com.taobao.arthas.core.command.monitor200.DashboardCommand;
-import com.taobao.arthas.core.command.monitor200.HeapDumpCommand;
-import com.taobao.arthas.core.command.monitor200.JvmCommand;
-import com.taobao.arthas.core.command.monitor200.MBeanCommand;
-import com.taobao.arthas.core.command.monitor200.MonitorCommand;
-import com.taobao.arthas.core.command.monitor200.PerfCounterCommand;
-import com.taobao.arthas.core.command.monitor200.ProfilerCommand;
-import com.taobao.arthas.core.command.monitor200.StackCommand;
-import com.taobao.arthas.core.command.monitor200.ThreadCommand;
-import com.taobao.arthas.core.command.monitor200.TimeTunnelCommand;
-import com.taobao.arthas.core.command.monitor200.TraceCommand;
-import com.taobao.arthas.core.command.monitor200.WatchCommand;
 import com.taobao.arthas.core.shell.command.AnnotatedCommand;
 import com.taobao.arthas.core.shell.command.Command;
 import com.taobao.arthas.core.shell.command.CommandResolver;
-import com.taobao.arthas.core.shell.handlers.command.CommandInterruptHandler;
-import io.netty.util.internal.ResourcesUtil;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
 import java.net.JarURLConnection;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import java.net.URL;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -182,11 +137,13 @@ public class BuiltinCommandPack implements CommandResolver {
                 // if is command add to the list
                 classes.add((Class<? extends AnnotatedCommand>) clazz);
             }
-        } catch (ClassNotFoundException e) {}
+        } catch (ClassNotFoundException e) {
+        }
     }
 
     /**
      * Meet the parent class is AnnotatedCommand
+     *
      * @param clazz
      * @return
      */
