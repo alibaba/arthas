503e0de6b4328d8541aaabbb233bff43f7563276
gongdewei <kylixs@qq.com>
2020-09-22 22:18:51 +0800

    improve dashboard/thread cpu time sample (#1501)
    * dashboard command support native thread, delta time
    * thread command support --all option

--- core/src/main/java/com/taobao/arthas/core/command/view/ThreadView.java
+++ core/src/main/java/com/taobao/arthas/core/command/view/ThreadView.java
@@ -1,6 +1,6 @@
 package com.taobao.arthas.core.command.view;
 
-import com.taobao.arthas.core.command.model.ThreadCpuInfo;
+import com.taobao.arthas.core.command.model.BusyThreadInfo;
 import com.taobao.arthas.core.command.model.ThreadModel;
 import com.taobao.arthas.core.command.model.ThreadVO;
 import com.taobao.arthas.core.shell.command.CommandProcess;
@@ -23,12 +23,12 @@ public class ThreadView extends ResultView<ThreadModel> {
     public void draw(CommandProcess process, ThreadModel result) {
         if (result.getThreadInfo() != null) {
             // no cpu usage info
-            String content = ThreadUtil.getFullStacktrace(result.getThreadInfo(), -1);
+            String content = ThreadUtil.getFullStacktrace(result.getThreadInfo());
             process.write(content);
         } else if (result.getBusyThreads() != null) {
-            ThreadCpuInfo[] threadInfos = result.getBusyThreads();
-            for (ThreadCpuInfo info : threadInfos) {
-                String stacktrace = ThreadUtil.getFullStacktrace(info.threadInfo(), info.getCpuUsage());
+            List<BusyThreadInfo> threadInfos = result.getBusyThreads();
+            for (BusyThreadInfo info : threadInfos) {
+                String stacktrace = ThreadUtil.getFullStacktrace(info, -1, -1);
                 process.write(stacktrace).write("\n");
             }
         } else if (result.getBlockingLockInfo() != null) {
@@ -44,6 +44,15 @@ public class ThreadView extends ResultView<ThreadModel> {
             for (Integer value : threadStateCount.values()) {
                 total += value;
             }
+
+            int internalThreadCount = 0;
+            for (ThreadVO thread : threadStats) {
+                if (thread.getId() <= 0) {
+                    internalThreadCount += 1;
+                }
+            }
+            total += internalThreadCount;
+
             StringBuilder threadStat = new StringBuilder();
             threadStat.append("Threads Total: ").append(total);
 
@@ -51,12 +60,20 @@ public class ThreadView extends ResultView<ThreadModel> {
                 Integer count = threadStateCount.get(s);
                 threadStat.append(", ").append(s.name()).append(": ").append(count);
             }
+            if (internalThreadCount > 0) {
+                threadStat.append(", Internal threads: ").append(internalThreadCount);
+            }
             String stat = RenderUtil.render(new LabelElement(threadStat), process.width());
 
             //thread stats
-            int height = Math.max(5, process.height() - 2);
-            //remove blank lines
-            height = Math.min(height, threadStats.size() + 2);
+            int height;
+            if (result.isAll()) {
+                height = threadStats.size() + 1;
+            } else {
+                height = Math.max(5, process.height() - 2);
+                //remove blank lines
+                height = Math.min(height, threadStats.size() + 2);
+            }
             String content = ViewRenderUtil.drawThreadInfo(threadStats, process.width(), height);
             process.write(stat + content);
         }
