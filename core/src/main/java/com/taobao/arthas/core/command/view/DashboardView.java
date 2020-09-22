7dd09b54b11fec6cbccb156b17d1f3810995ad32
gongdewei <kylixs@qq.com>
2020-09-22 14:32:28 +0800

    Improve dashboard height division

--- core/src/main/java/com/taobao/arthas/core/command/view/DashboardView.java
+++ core/src/main/java/com/taobao/arthas/core/command/view/DashboardView.java
@@ -28,7 +28,17 @@ public class DashboardView extends ResultView<DashboardModel> {
         // 上半部分放thread top。下半部分再切分为田字格，其中上面两格放memory, gc的信息。下面两格放tomcat,
         // runtime的信息
         int totalHeight = height - 1;
-        int threadTopHeight = totalHeight / 2;
+        int threadTopHeight;
+        if (totalHeight <= 24) {
+            //总高度较小时取1/2
+            threadTopHeight = totalHeight / 2;
+        } else {
+            //总高度较大时取1/3，但不少于上面的值(24/2=12)
+            threadTopHeight = totalHeight / 3;
+            if (threadTopHeight < 12) {
+                threadTopHeight = 12;
+            }
+        }
         int lowerHalf = totalHeight - threadTopHeight;
 
         //Memory至少保留8行, 显示metaspace信息
@@ -50,7 +60,8 @@ public class DashboardView extends ResultView<DashboardModel> {
         }
 
         //如果MemoryInfo高度有剩余，则增大ThreadHeight
-        memoryInfoHeight = Math.min(memoryInfoHeight, getMemoryInfoHeight(result.getMemoryInfo()));
+        int maxMemoryInfoHeight = getMemoryInfoHeight(result.getMemoryInfo());
+        memoryInfoHeight = Math.min(memoryInfoHeight, maxMemoryInfoHeight);
         threadTopHeight = totalHeight - memoryInfoHeight - runtimeInfoHeight;
 
         String threadInfo = ViewRenderUtil.drawThreadInfo(result.getThreads(), width, threadTopHeight);
