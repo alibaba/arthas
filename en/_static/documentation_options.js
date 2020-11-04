2f5ab892a61419f6b7b98414190697ba61c742b5
hengyunabc <hengyunabc@gmail.com>
2020-11-04 15:35:30 +0800

    update doc

--- en/_static/documentation_options.js
+++ en/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.3',
+    VERSION: '${revision}',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
