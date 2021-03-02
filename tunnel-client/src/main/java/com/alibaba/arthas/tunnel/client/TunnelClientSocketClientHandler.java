985963b9d1272f2a872810a57d69291fb1294c81
hengyunabc <hengyunabc@gmail.com>
2021-03-02 19:30:05 +0800

    tunnel client close ctx in exceptionCaught. #1720

--- tunnel-client/src/main/java/com/alibaba/arthas/tunnel/client/TunnelClientSocketClientHandler.java
+++ tunnel-client/src/main/java/com/alibaba/arthas/tunnel/client/TunnelClientSocketClientHandler.java
@@ -164,9 +164,10 @@ public class TunnelClientSocketClientHandler extends SimpleChannelInboundHandler
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        logger.error("TunnelClient error, tunnel server url: " + tunnelClient.getTunnelServerUrl(), cause);
         if (!registerPromise.isDone()) {
             registerPromise.setFailure(cause);
         }
-        ctx.fireExceptionCaught(cause);
+        ctx.close();
     }
 }
