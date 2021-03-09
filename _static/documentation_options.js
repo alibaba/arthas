328ac79bed1a6dfb55df3e0931d8083bd7408061
hengyunabc <hengyunabc@gmail.com>
2021-03-09 15:29:44 +0800

    update doc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.8',
+    VERSION: '3.5.0',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
