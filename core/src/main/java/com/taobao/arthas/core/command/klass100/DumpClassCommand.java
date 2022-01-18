dea09e07f6bc22ee05c60ea2136ddff3808770d7
67 <67@gd67.com>
2022-01-18 15:24:28 +0800

    change RetransformCommand and DumpClassCommand limit option description to match @DefaultValue annotation

--- core/src/main/java/com/taobao/arthas/core/command/klass100/DumpClassCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/klass100/DumpClassCommand.java
@@ -92,7 +92,7 @@ public class DumpClassCommand extends AnnotatedCommand {
     }
 
     @Option(shortName = "l", longName = "limit")
-    @Description("The limit of dump classes size, default value is 5")
+    @Description("The limit of dump classes size, default value is 50")
     @DefaultValue("50")
     public void setLimit(int limit) {
         this.limit = limit;
