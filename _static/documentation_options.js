95f207f409324ebcffce48e2a31943d12a0a333c
hengyunabc <hengyunabc@gmail.com>
2022-03-25 21:32:33 +0800

    update doc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.5.6',
+    VERSION: '3.6.0',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
