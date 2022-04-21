225f34bdd9722ebef0cbb995b0243c4d6a22b7c0
yqxzy <84971201+yqxzy@users.noreply.github.com>
2022-04-21 22:04:50 +0800

    Update Decompiler.java
    fix issue#1963

--- core/src/main/java/com/taobao/arthas/core/util/Decompiler.java
+++ core/src/main/java/com/taobao/arthas/core/util/Decompiler.java
@@ -128,7 +128,43 @@ public class Decompiler {
         for (String line : lines) {
             Integer srcLineNumber = lineMapping.get(index + 1);
             if (srcLineNumber != null) {
-                sb.append(String.format(formatStr, srcLineNumber));
+                 private static String addLineNumber(String src, Map<Integer, Integer> lineMapping) {
+        int maxLineNumber = 0;
+        for (Integer value : lineMapping.values()) {
+            if (value != null && value > maxLineNumber) {
+                maxLineNumber = value;
+            }
+        }
+
+        String formatStr = "/*%2d*/ ";
+        String emptyStr =  "       ";
+
+        StringBuilder sb = new StringBuilder();
+
+        List<String> lines = StringUtils.toLines(src);
+
+        if (maxLineNumber >= 1000) {
+            formatStr = "/*%4d*/ ";
+            emptyStr =  "         ";
+        } else if (maxLineNumber >= 100) {
+            formatStr = "/*%3d*/ ";
+            emptyStr =  "        ";
+        }
+
+        int index = 0;
+        for (String line : lines) {
+            Integer srcLineNumber = lineMapping.get(index + 1);
+            if (srcLineNumber != null) {
+                sb.append(String.format(formatStr, (index+1)));
+            } else {
+                sb.append(emptyStr);
+            }
+            sb.append(line).append("\n");
+            index++;
+        }
+
+        return sb.toString();
+    }
             } else {
                 sb.append(emptyStr);
             }
