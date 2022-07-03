d5d572809559311425753f462e8a12773dd4dc43
QAQ <13435638964@163.com>
2022-07-03 16:42:58 +0800

    chore:change icon

--- web-console/src/main/arthasWebConsole/src/App.vue
+++ web-console/src/main/arthasWebConsole/src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" src="./assets/arthas.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
