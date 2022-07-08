792a3de64b771217c537617b5d9545b32fe4f200
Fatpandac <i@Fatpandac.com>
2022-07-09 00:27:24 +0800

    docs(config): add contributors data from old docs

--- site-vuepress/docs/.vuepress/client.js
+++ site-vuepress/docs/.vuepress/client.js
@@ -1,4 +1,36 @@
 import { defineClientConfig } from "@vuepress/client";
+import { usePageData } from "@vuepress/client";
+import oldContributorsData from "./oldContributorsData.json";
+
+const addOldDocsContributors = () => {
+  const page = usePageData();
+  if (!page.value.git) return;
+  const filePath = page.value.filePathRelative;
+  const contributors = page.value.git.contributors;
+  const oldContributors = oldContributorsData[filePath];
+
+  const haveSameContributor = (contributors, oldContributor) => {
+    return contributors.find(
+      (contributor) =>
+        contributor.name === oldContributor.name &&
+        contributor.email === oldContributor.email
+    );
+  };
+
+  if (oldContributors) {
+    oldContributors.forEach((oldContributor) => {
+      if (!haveSameContributor(contributors, oldContributor)) {
+        contributors.push(oldContributor);
+      } else {
+        haveSameContributor(contributors, oldContributor).commits +=
+          oldContributor.commits;
+      }
+    });
+  }
+
+  // sort contributors by commits
+  contributors?.sort((a, b) => b.commits - a.commits);
+};
 
 export default defineClientConfig({
   enhance({ router }) {
@@ -13,6 +45,11 @@ export default defineClientConfig({
     router.addRoute({
       path: "/doc/en/:path*",
       redirect: (to) => `/en/doc${to.fullPath.replace("/doc/en", "")}`,
+    });
+    router.afterEach((to, from) => {
+      if (to.fullPath !== from.fullPath) {
+        addOldDocsContributors();
+      }
     });
   },
 });
