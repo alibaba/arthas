a2c41b8a6738c37f99624d2596bd05e743a0ca97
Fatpandac <i@fatpandac.com>
2022-07-18 23:51:38 +0800

    docs(config): add meta

--- site/docs/.vuepress/configs/head.js
+++ site/docs/.vuepress/configs/head.js
@@ -4,6 +4,38 @@ module.exports = [
     "meta",
     { name: "viewport", content: "width=device-width, initial-scale=1.0" },
   ],
+  ["meta", { property: "og:title", content: "Arthas" }],
+  [
+    "meta",
+    {
+      property: "og:image:alt",
+      content:
+        "Alibaba Java Diagnostic Tool Arthas/Alibaba Java诊断利器Arthas - Fatpandac/arthas: Alibaba Java Diagnostic Tool Arthas/Alibaba Java诊断利器Arthas",
+    },
+  ],
+  ["meta", { property: "og:image", content: "/images/arthas_mate_image.png" }],
+  [
+    "meta",
+    {
+      property: "og:description",
+      content:
+        "Alibaba Java Diagnostic Tool Arthas/Alibaba Java诊断利器Arthas - Fatpandac/arthas: Alibaba Java Diagnostic Tool Arthas/Alibaba Java诊断利器Arthas",
+    },
+  ],
+  ["meta", { property: "og:image:width", content: "1200" }],
+  ["meta", { property: "og:image:height", content: "600" }],
+  [
+    "meta",
+    { property: "twitter:image:src", content: "/images/arthas_mate_image.png" },
+  ],
+  [
+    "meta",
+    {
+      property: "twitter:image:alt",
+      content:
+        "Alibaba Java Diagnostic Tool Arthas/Alibaba Java诊断利器Arthas - Fatpandac/arthas: Alibaba Java Diagnostic Tool Arthas/Alibaba Java诊断利器Arthas",
+    },
+  ],
   [
     "script",
     {},
