5fb0b1b8f49f2ee088d1d4acbbbc588ade43574a
Fatpandac <i@fatpandac.com>
2022-08-02 22:55:15 +0800

    fix: format

--- site/docs/.vuepress/plugins/vuepress-plugin-loadVersion/index.js
+++ site/docs/.vuepress/plugins/vuepress-plugin-loadVersion/index.js
@@ -3,8 +3,6 @@ const fetch = require("node-fetch");
 const convert = require("xml-js");
 
 exports.loadVersionPlugin = () => {
-  var version;
-
   const data = fs.readFileSync("../pom.xml");
   const pom = convert.xml2js(data.toString(), { compact: true });
 
@@ -16,13 +14,14 @@ exports.loadVersionPlugin = () => {
       .then((res) => res.response.docs[0].latestVersion);
   };
 
-  version = pom.project.properties.revision._text;
+  const version = pom.project.properties.revision._text;
 
   return {
     name: "vuepress-plugin-loadVersion",
     extendsPage: async (page) => {
-      const injectPagePaths = ["/", "/en/"];
-      if (!injectPagePaths.includes(page.data.path)) return;
+      const injectVersionPagePaths = ["/", "/en/"];
+
+      if (!injectVersionPagePaths.includes(page.data.path)) return;
 
       if (version.includes("SNAPSHOT")) {
         page.data.version = await getVersionByMaven();
