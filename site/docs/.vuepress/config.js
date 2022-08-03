2f06d8bb36efda16c31e7acfab9acf4c65e0495e
Fatpandac <i@fatpandac.com>
2022-08-03 16:39:59 +0800

    docs(style/plugin): add a version tag to the navbar and add the DocSearch plugin (#2233)

--- site/docs/.vuepress/config.js
+++ site/docs/.vuepress/config.js
@@ -1,8 +1,10 @@
 const { localTheme } = require("./theme/index");
+const { loadVersionPlugin } = require("./plugins/vuepress-plugin-loadVersion");
 
 const {
   activeHeaderLinksPlugin,
 } = require("@vuepress/plugin-active-header-links");
+const { docsearchPlugin } = require("@vuepress/plugin-docsearch");
 const { copyCodePlugin } = require("vuepress-plugin-copy-code2");
 const { redirectPlugin } = require("vuepress-plugin-redirect");
 const { searchPlugin } = require("@vuepress/plugin-search");
@@ -111,7 +113,57 @@ module.exports = {
     }),
     activeHeaderLinksPlugin({
       headerLinkSelector: "div.right-menu-item > a",
-      delay: 100,
+      delay: 0,
     }),
+    docsearchPlugin({
+      apiKey: "03fb4b6577b57b5dafc792d9ddf66508",
+      indexName: "arthas",
+      appId: "UX8WBNVHHR",
+      locales: {
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
+    }),
+    // Local plugin
+    loadVersionPlugin(),
   ],
 };
---
+++ site/docs/.vuepress/plugins/vuepress-plugin-loadVersion/index.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+const convert = require("xml-js");
+
+exports.loadVersionPlugin = () => {
+  const data = fs.readFileSync("../pom.xml");
+  const pom = convert.xml2js(data.toString(), { compact: true });
+
+  const getVersionByMaven = async () => {
+    return await fetch(
+      "https://search.maven.org/solrsearch/select?q=arthas&rows=1&wt=json"
+    )
+      .then((res) => res.json())
+      .then((res) => res.response.docs[0].latestVersion);
+  };
+
+  const version = pom.project.properties.revision._text;
+
+  return {
+    name: "vuepress-plugin-loadVersion",
+    extendsPage: async (page) => {
+      const injectVersionPagePaths = ["/", "/en/"];
+
+      if (!injectVersionPagePaths.includes(page.data.path)) return;
+
+      if (version.includes("SNAPSHOT")) {
+        page.data.version = await getVersionByMaven();
+      } else {
+        page.data.version = version;
+      }
+    },
+  };
+};
