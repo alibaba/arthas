b269ba5a572c2a6c705569fd44be5736e7c4e998
hengyunabc <hengyunabc@gmail.com>
2022-08-25 17:56:15 +0800

    release 3.6.5

--- bin/as.sh
+++ bin/as.sh
@@ -8,10 +8,10 @@
 
 # program : Arthas
 #  author : Core Engine @ Taobao.com
-#    date : 2022-07-30
+#    date : 2022-08-25
 
 # current arthas script version
-ARTHAS_SCRIPT_VERSION=3.6.4
+ARTHAS_SCRIPT_VERSION=3.6.5
 
 # SYNOPSIS
 #   rreadlink <fileOrDirPath>
@@ -453,7 +453,7 @@ EXAMPLES:
   ./as.sh --stat-url 'http://192.168.10.11:8080/api/stat'
   ./as.sh -c 'sysprop; thread' <pid>
   ./as.sh -f batch.as <pid>
-  ./as.sh --use-version 3.6.4
+  ./as.sh --use-version 3.6.5
   ./as.sh --session-timeout 3600
   ./as.sh --attach-only
   ./as.sh --disabled-commands stop,dump
