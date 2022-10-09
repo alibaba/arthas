1646ac0ae575ff30389ac805962b06950961921b
QAQ <13435638964@163.com>
2022-10-09 23:30:51 +0800

    -amend

--- web-ui/arthasWebConsole/vite.config.ts
+++ web-ui/arthasWebConsole/vite.config.ts
@@ -1,24 +1,32 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import * as path from "path";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue({
     reactivityTransform: path.resolve(__dirname, "./ui"),
   })],
+  // root:path.resolve(__dirname,"src"),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./ui/src/"),
-      "vue": "vue/dist/vue.esm-bundler.js",
+      "@": path.resolve(__dirname, "./ui/src/")
     },
   },
   build: {
+    emptyOutDir:true,
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),
         ui: path.resolve(__dirname, "ui/index.html"),
       },
+      
+      output:{
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames:'static/js/[name]-[hash].js',
+        assetFileNames:'static/[ext]/[name]-[hash].[ext]'
+      }
     },
   },
   base: "/",
