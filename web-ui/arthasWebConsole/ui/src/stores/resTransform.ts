5f7f952f5708922b96e8ab6d9cc339555a911bb6
辗转反侧的咸鱼 <13435638964@163.com>
2022-10-31 19:35:30 +0800

    perf(web ui):remove tree component in watch

--- web-ui/arthasWebConsole/ui/src/stores/resTransform.ts
+++ web-ui/arthasWebConsole/ui/src/stores/resTransform.ts
@@ -4,6 +4,6 @@ export const transfromStore = defineStore("transformStore", {
   actions: {
     transformStackTrace(trace: StackTrace) {
       return `${trace.className}.${trace.methodName} (${trace.fileName}: ${trace.lineNumber})`;
-    },
+    }
   },
 });
