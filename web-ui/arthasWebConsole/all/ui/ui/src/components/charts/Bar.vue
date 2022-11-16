0eb7354267dac6b7233576a6eda4a2faf6946a28
辗转反侧的咸鱼 <13435638964@163.com>
2022-11-17 00:29:12 +0800

    refactor(web ui):refactor echarts component

---
+++ web-ui/arthasWebConsole/all/ui/ui/src/components/charts/Bar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Chart from "./Base.vue"
+import * as echarts from 'echarts/core';
+import { DatasetComponentOption, DataZoomComponent, DataZoomComponentOption, GridComponent, GridComponentOption, LegendComponent, LegendComponentOption, TitleComponentOption, ToolboxComponentOption, TooltipComponentOption } from 'echarts/components';
+import { BarChart, BarSeriesOption, } from 'echarts/charts';
+import { LabelLayout } from 'echarts/features';
+import { BarChartOption } from "@/echart";
+let useList = ([
+  // LegendComponent,
+  BarChart,
+  LabelLayout,
+  GridComponent,
+  DataZoomComponent
+]);
+const props = defineProps<{ option: BarChartOption }>()
+</script>
+
+<template>
+  <Chart :use-list="useList" :option="props.option"></Chart>
+</template>
---
+++ web-ui/arthasWebConsole/all/ui/ui/src/echart.d.ts
@@ -0,0 +1,47 @@
+import * as echarts from "echarts";
+import {
+  DatasetComponentOption,
+  DataZoomComponentOption,
+  GridComponentOption,
+  LegendComponentOption,
+  TitleComponentOption,
+  ToolboxComponentOption,
+  TooltipComponentOption,
+} from "echarts/components";
+import {
+  BarSeriesOption,
+  LineSeriesOption,
+  PieSeriesOption,
+} from "echarts/charts";
+type LineChartOption = echarts.ComposeOption<
+  | TooltipComponentOption
+  | TitleComponentOption
+  | DatasetComponentOption
+  | LineSeriesOption
+  | TitleComponentOption
+  | ToolboxComponentOption
+  | TooltipComponentOption
+  | GridComponentOption
+  | LegendComponentOption
+  | DataZoomComponentOption
+  | LineSeriesOption
+>;
+
+type BarChartOption = echarts.ComposeOption<
+  | TooltipComponentOption
+  | TitleComponentOption
+  | DatasetComponentOption
+  | BarSeriesOption
+  | DataZoomComponentOption
+  | LegendComponentOption
+  | GridComponentOption
+  | ToolboxComponentOption
+>;
+
+type CircleChartOption = echarts.ComposeOption<
+  | TooltipComponentOption
+  | TitleComponentOption
+  | DatasetComponentOption
+  | PieSeriesOption
+  | LegendComponentOption
+>;
