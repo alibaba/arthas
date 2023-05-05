9a6f74a64c8ac82819c7170c958fdca13902c52f
codeleep <56161468+codeleep@users.noreply.github.com>
2023-05-05 16:30:08 +0800

    UserStat support agentId (#2501)

--- tunnel-server/src/main/java/com/alibaba/arthas/tunnel/server/app/web/StatController.java
+++ tunnel-server/src/main/java/com/alibaba/arthas/tunnel/server/app/web/StatController.java
@@ -23,10 +23,11 @@ public class StatController {
     @ResponseBody
     public Map<String, Object> execute(@RequestParam(value = "ip", required = true) String ip,
             @RequestParam(value = "version", required = true) String version,
+            @RequestParam(value = "agentId", required = false) String agentId,
             @RequestParam(value = "command", required = true) String command,
             @RequestParam(value = "arguments", required = false, defaultValue = "") String arguments) {
 
-        logger.info("arthas stat, ip: {}, version: {}, command: {}, arguments: {}", ip, version, command, arguments);
+        logger.info("arthas stat, ip: {}, version: {}, agentId: {}, command: {}, arguments: {}", ip, version, agentId, command, arguments);
 
         Map<String, Object> result = new HashMap<>();
 
