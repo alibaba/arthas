e3855cde50a50313a7f8a4c8f3895eeeb0958f33
Fatpandac <i@fatpandac.com>
2023-07-22 15:56:20 +0800

    feat: change to use docker setup java

--- arthas-tutorials-cn/command/heapdump/foreground.sh
+++ arthas-tutorials-cn/command/heapdump/foreground.sh
@@ -1,3 +1,6 @@
 echo "设置环境..."
-sudo apt-get install openjdk-11-jdk -y &> /dev/null
-echo "完成"
+docker create -i -p80:80 -p8563:8563 -p3658:3658 --name arthas openjdk:11 bash
+docker start arthas
+echo docker exec -it arthas bash >> ~/.bashrc
+clear
+docker exec -it arthas bash
