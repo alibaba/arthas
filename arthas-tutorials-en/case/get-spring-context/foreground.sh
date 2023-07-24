2e2fc129904097b4ee83319785b5533f6cd7d1fe
Fatpandac <i@fatpandac.com>
2023-07-24 12:35:28 +0800

    feat: add get spring context and boot details case

---
+++ arthas-tutorials-en/case/get-spring-context/foreground.sh
@@ -0,0 +1,6 @@
+echo "Setup environment..."
+docker create -i -p80:80 -p8563:8563 -p3658:3658 --name arthas openjdk:11 bash
+docker start arthas
+echo docker exec -it arthas bash >> ~/.bashrc
+clear
+docker exec -it arthas bash
