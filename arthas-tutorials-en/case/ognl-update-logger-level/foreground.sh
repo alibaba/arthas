32318be4ead6c6ca834a6112da0019aa64efcb46
Fatpandac <i@fatpandac.com>
2023-07-25 17:17:49 +0800

    feat: change to use minimal image

--- arthas-tutorials-en/case/ognl-update-logger-level/foreground.sh
+++ arthas-tutorials-en/case/ognl-update-logger-level/foreground.sh
@@ -1,5 +1,5 @@
 echo "Setup environment..."
-docker create -i -p80:80 -p8563:8563 -p3658:3658 --name arthas openjdk:11 bash
+docker create -i -p80:80 -p8563:8563 -p3658:3658 --name arthas openjdk:11-jdk-stretch bash
 docker start arthas
 echo docker exec -it arthas bash >> ~/.bashrc
 clear
