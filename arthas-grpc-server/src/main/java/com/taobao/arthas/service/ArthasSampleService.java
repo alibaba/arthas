c1a9ea87b2e44e35c177b0c960be82262aff4f0c
summer <1129126684@qq.com>
2024-07-01 00:39:36 +0800

    init

---
+++ arthas-grpc-server/src/main/java/com/taobao/arthas/service/ArthasSampleService.java
@@ -0,0 +1,13 @@
+package com.taobao.arthas.service;/**
+ * @author: 風楪
+ * @date: 2024/6/30 下午11:42
+ */
+
+/**
+ * @author: FengYe
+ * @date: 2024/6/30 下午11:42
+ * @description: ArthasSampleService
+ */
+public interface ArthasSampleService {
+    String trace(String command);
+}
---
+++ arthas-grpc-server/src/main/java/com/taobao/arthas/service/ArthasSampleServiceImpl.java
@@ -0,0 +1,12 @@
+package com.taobao.arthas.service;/**
+ * @author: 風楪
+ * @date: 2024/6/30 下午11:43
+ */
+
+/**
+ * @author: FengYe
+ * @date: 2024/6/30 下午11:43
+ * @description: ArthasSampleServiceImpl
+ */
+public class ArthasSampleServiceImpl {
+}
