61cdb9a57d3b3d1ea0ddcaad87422905f1a2db7f
summer <1129126684@qq.com>
2024-06-30 19:21:36 +0800

    init

---
+++ arthas-grpc-server/src/main/java/com/taobao/arthas/Main.java
@@ -0,0 +1,27 @@
+package com.taobao.arthas;
+
+import com.taobao.arthas.service.impl.HelloServiceImpl;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+/**
+ * @author: 風楪
+ * @date: 2024/6/30 上午1:22
+ */
+public class Main {
+    public static void main(String[] args) {
+        int port = 9090;
+        Server server = ServerBuilder.forPort(port)
+                .addService(new HelloServiceImpl())
+                .build();
+        try {
+            server.start();
+            System.out.println("Server started, listening on " + port);
+            server.awaitTermination();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
