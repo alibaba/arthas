30fe6897cc84688462a89ef4b3a8d714fdb82b03
taokankan.tk <taokan1991@aliyun.com>
2024-10-08 19:25:59 +0800

    【支持qlexpress表达式】

--- core/src/main/java/com/taobao/arthas/core/command/express/QLExpress.java
+++ core/src/main/java/com/taobao/arthas/core/command/express/QLExpress.java
@@ -2,7 +2,7 @@ package com.taobao.arthas.core.command.express;
 
 import com.alibaba.arthas.deps.org.slf4j.Logger;
 import com.alibaba.arthas.deps.org.slf4j.LoggerFactory;
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.qlexpress4.Express4Runner;
 import com.alibaba.qlexpress4.InitOptions;
 import com.alibaba.qlexpress4.QLOptions;
