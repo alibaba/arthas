8e4dd907ff26c429b4d0712f5bc22d98adfd58c3
hengyunabc <hengyunabc@gmail.com>
2024-11-27 00:41:28 +0800

    log profiler execute args

--- core/src/main/java/com/taobao/arthas/core/command/monitor200/ProfilerCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/monitor200/ProfilerCommand.java
@@ -714,6 +714,7 @@ public class ProfilerCommand extends AnnotatedCommand {
 
     private static String execute(AsyncProfiler asyncProfiler, String arg)
             throws IllegalArgumentException, IOException {
+        logger.info("profiler execute args: {}", arg);
         String result = asyncProfiler.execute(arg);
         if (!result.endsWith("\n")) {
             result += "\n";
