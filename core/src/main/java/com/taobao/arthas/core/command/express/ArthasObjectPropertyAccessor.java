25992ae8c098c14016b655536facb9db2fe3c8b1
Allan-QLB <qin_libin@foxmail.com>
2024-11-27 18:40:29 +0800

    解决冲突

--- core/src/main/java/com/taobao/arthas/core/command/express/ArthasObjectPropertyAccessor.java
+++ core/src/main/java/com/taobao/arthas/core/command/express/ArthasObjectPropertyAccessor.java
@@ -7,16 +7,18 @@ import ognl.OgnlContext;
 import ognl.OgnlException;
 import ognl.OgnlRuntime;
 
+import java.util.Map;
+
 /**
  * @author hengyunabc 2022-03-24
  */
 public class ArthasObjectPropertyAccessor extends ObjectPropertyAccessor {
 
     @Override
-    public Object getPossibleProperty(OgnlContext context, Object target, String name) throws OgnlException {
+    public Object getPossibleProperty(Map context, Object target, String name) throws OgnlException {
         Object result;
         try {
-            result = OgnlRuntime.getFieldValue(context, target, name, true);
+            result = OgnlRuntime.getFieldValue((OgnlContext) context, target, name, true);
         } catch (Exception ex) {
             throw new OgnlException(name, ex);
         }
@@ -25,7 +27,7 @@ public class ArthasObjectPropertyAccessor extends ObjectPropertyAccessor {
     }
 
     @Override
-    public Object setPossibleProperty(OgnlContext context, Object target, String name, Object value) throws OgnlException {
+    public Object setPossibleProperty(Map context, Object target, String name, Object value) throws OgnlException {
         if (GlobalOptions.strict) {
             throw new IllegalAccessError(GlobalOptions.STRICT_MESSAGE);
         }
