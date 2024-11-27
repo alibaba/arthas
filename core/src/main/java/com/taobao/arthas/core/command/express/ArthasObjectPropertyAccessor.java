9a22a65e6494b2ea49dd1711fe509b4a4ea300af
Allan-QLB <qin_libin@foxmail.com>
2024-11-27 17:51:48 +0800

    修复ognl表达式获取字段值返回类型错误

--- core/src/main/java/com/taobao/arthas/core/command/express/ArthasObjectPropertyAccessor.java
+++ core/src/main/java/com/taobao/arthas/core/command/express/ArthasObjectPropertyAccessor.java
@@ -5,11 +5,24 @@ import com.taobao.arthas.core.GlobalOptions;
 import ognl.ObjectPropertyAccessor;
 import ognl.OgnlContext;
 import ognl.OgnlException;
+import ognl.OgnlRuntime;
 
 /**
  * @author hengyunabc 2022-03-24
  */
 public class ArthasObjectPropertyAccessor extends ObjectPropertyAccessor {
+
+    @Override
+    public Object getPossibleProperty(OgnlContext context, Object target, String name) throws OgnlException {
+        Object result;
+        try {
+            result = OgnlRuntime.getFieldValue(context, target, name, true);
+        } catch (Exception ex) {
+            throw new OgnlException(name, ex);
+        }
+
+        return result;
+    }
 
     @Override
     public Object setPossibleProperty(OgnlContext context, Object target, String name, Object value) throws OgnlException {
