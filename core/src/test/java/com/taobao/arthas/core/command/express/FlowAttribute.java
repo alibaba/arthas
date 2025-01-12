dac03f5c65654269c1ee27754bb3805b431d3736
taokankan.tk <taokan1991@aliyun.com>
2025-01-12 20:06:51 +0800

    【支持qlexpress表达式】冲突

---
+++ core/src/test/java/com/taobao/arthas/core/command/express/FlowAttribute.java
@@ -0,0 +1,9 @@
+package com.taobao.arthas.core.command.express;
+
+public class FlowAttribute {
+    private String bxApp = "aaa";
+
+    public String getBxApp() {
+     return this.bxApp ;
+    }
+}
