091ce30f314156990fcd54b595595cc6f8cf484e
hengyunabc <hengyunabc@gmail.com>
2025-02-14 16:50:00 +0800

    improve banner, support app_name, start_time. #2990

--- core/src/main/java/com/taobao/arthas/core/util/ArthasBanner.java
+++ core/src/main/java/com/taobao/arthas/core/util/ArthasBanner.java
@@ -112,13 +112,25 @@ public class ArthasBanner {
 
     public static String welcome(Map<String, String> infos) {
         logger.info("Current arthas version: {}, recommend latest version: {}", version(), latestVersion());
+        String appName = System.getProperty("project.name");
+        if (appName == null) {
+            appName = System.getProperty("app.name");
+        }
+        if (appName == null) {
+            appName = System.getProperty("spring.application.name");
+        }
         TableElement table = new TableElement().rightCellPadding(1)
                         .row("wiki", wiki())
                         .row("tutorials", tutorials())
                         .row("version", version())
-                        .row("main_class", PidUtils.mainClass())
-                        .row("pid", PidUtils.currentPid())
-                        .row("time", DateUtils.getCurrentDateTime());
+                        .row("main_class", PidUtils.mainClass());
+
+        if (appName != null) {
+            table.row("app_name", appName);
+        }
+        table.row("pid", PidUtils.currentPid())
+             .row("start_time", DateUtils.getStartDateTime())
+             .row("currnt_time", DateUtils.getCurrentDateTime());
         for (Entry<String, String> entry : infos.entrySet()) {
             table.row(entry.getKey(), entry.getValue());
         }
