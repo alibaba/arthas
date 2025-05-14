d9811a07b67a24a3daff8031fca85eda1a354211
hengyunabc <hengyunabc@gmail.com>
2025-05-14 10:53:27 +0800

    fix ArthasBanner latestVersion() block. #3021

--- core/src/main/java/com/taobao/arthas/core/util/ArthasBanner.java
+++ core/src/main/java/com/taobao/arthas/core/util/ArthasBanner.java
@@ -139,14 +139,29 @@ public class ArthasBanner {
     }
 
     static String latestVersion() {
+        final String[] version = { "" };
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URLConnection urlConnection = openURLConnection(ARTHAS_LATEST_VERSIONS_URL);
+                    InputStream inputStream = urlConnection.getInputStream();
+                    version[0] = com.taobao.arthas.common.IOUtils.toString(inputStream).trim();
+                } catch (Throwable e) {
+                    logger.debug("get latest version error", e);
+                }
+            }
+        });
+
+        thread.setDaemon(true);
+        thread.start();
         try {
-            URLConnection urlConnection = openURLConnection(ARTHAS_LATEST_VERSIONS_URL);
-            InputStream inputStream = urlConnection.getInputStream();
-            return com.taobao.arthas.common.IOUtils.toString(inputStream).trim();
+            thread.join(2000); // Wait up to 2 seconds for the version check
         } catch (Throwable e) {
-            // ignore
+            // Ignore
         }
-        return "";
+
+        return version[0];
     }
 
     /**
