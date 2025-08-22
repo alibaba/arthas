25a93537b36bbcb9ba2f3e161590ffcd680715cb
dplcwj <1599838698@qq.com>
2025-08-22 20:26:03 +0800

    chore: modify jfr analysis backend pom file

--- labs/arthas-jfr-backend/src/main/java/org/example/jfranalyzerbackend/JfrAnalyzerBackendApplication.java
+++ labs/arthas-jfr-backend/src/main/java/org/example/jfranalyzerbackend/JfrAnalyzerBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JfrAnalyzerBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JfrAnalyzerBackendApplication.class, args);
     }
