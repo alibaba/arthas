e352e2f9c8e8eefbd3204481625c08fdda8ebc64
hengyunabc <hengyunabc@gmail.com>
2026-04-23 17:59:52 +0800

    Add demo external command integration test

---
+++ arthas-external-command-integration-test/src/test/java/com/taobao/arthas/externalcommand/it/TargetJvmApp.java
@@ -0,0 +1,10 @@
+package com.taobao.arthas.externalcommand.it;
+
+public class TargetJvmApp {
+
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            Thread.sleep(1000L);
+        }
+    }
+}
